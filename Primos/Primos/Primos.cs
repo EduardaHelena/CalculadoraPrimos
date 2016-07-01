65278ddca4e5b7eea3b2e2bc85de7377a644d8b4
Eduarda Helena <dudinhahelenaa@gmail.com>
2016-07-01 10:53:09 -0300

    CommitDosPrimos

---
+++ Primos/Primos/Primos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+        
+    class Primos
+    {
+            public int limit = 100000;
+            public List<int> numbers = new List<int>();
+
+            public int currentPosition = -1;
+
+            public void initNumbers(int limit)
+            {
+                for (int i = 2; i <= limit; i++)
+                {
+                    numbers.Add(i);
+                   Console.Write(i.ToString() + "; ");
+                }
+
+
+            }
+
+            public void markAndDelete()
+            {
+                currentPosition++;
+                if (numbers.Count > currentPosition)
+                {
+                    int currentValue = numbers.ElementAt(currentPosition);
+                    for (int i = currentPosition + 1; i < numbers.Count; i++)
+                    {
+                        if (numbers.ElementAt(i) % currentValue == 0) // não é primo
+                        {
+                            numbers.RemoveAt(i);
+                            Console.WriteLine(i.ToString() + "; ");
+                        }
+                    }
+                  
+                    markAndDelete();
+                }
+            }
+            public void numbersAsString()
+            {
+                
+                foreach (var item in numbers)
+                {
+                    Console.Write(item.ToString() + "; ");
+                }
+                Espera();
+            }
+
+            public void Espera()
+            {
+                Console.ReadKey();
+            }
+
+        
+        
+
+
+    }
+}
---
+++ Primos/Primos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primos p = new Primos();
+
+            Console.WriteLine("Até que numero você deseja?");
+            p.limit = int.Parse(Console.ReadLine());
+            
+
+            p.initNumbers(p.limit);
+
+            p.markAndDelete ();
+
+            p.numbersAsString ();
+
+
+
+        }
+    }
+}
